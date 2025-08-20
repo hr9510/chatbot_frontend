d8cc81c075033eb437e6a2ffec23d80cf0e940bf
himanshu <himanshurajoriya9510@gmail.com>
2025-08-20 16:36:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [queries, setQueries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false)
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  if (!query.trim()) return;
+
+  setLoading(true);
+
+  try {
+    const res = await fetch("http://localhost:5000/setUserQuery", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user_query: query }),
+  });
+
+    const data = await res.json();
+
+    setQueries((prev) => [data, ...prev]);
+    setQuery("");
+  } catch (error) {
+    console.error("Error submitting query:", error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+const fetchQueries = async () => {
+  try {
+    const res = await fetch("http://localhost:5000/getUserQueries");
+    const data = await res.json();
+    setQueries(data.reverse());
+  } catch (error) {
+    console.error("Error fetching queries:", error);
+  }
+};
+
+
+  return (
+    <div className="app">
+      <h1>Welcome to the ChatBOT Application!</h1>
+
+      {/* Query Form */}
+      <form onSubmit={handleSubmit} className="query-form">
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          type="text"
+          placeholder="Ask Anything..."
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Sending..." : "Send"}
+        </button>
+      </form>
+
+      <button onClick={()=>{fetchQueries() , setShow(!show)}} className="fetch-btn">
+        {show?"HIDE":"Get"} All Queries
+      </button>
+
+      {/* Display Queries */}
+      {show ? <div className="queries">
+        {queries.length > 0 ? (
+          queries.map((item, index) => (
+            <div key={index} className="query-card">
+              <p><strong>Question:</strong> {item.user_query}</p>
+              <p><strong>Answer:</strong> {item.query_answer}</p>
+            </div>
+          ))
+        ) : (
+          <p>No queries yet. Ask something!</p>
+        )}
+      </div>
+        : 
+      ""}
+    </div>
+  );
+}
+
+export default App;
